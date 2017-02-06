625ab078b6ec0929b8cd9a21f9e4e31de8754642
kathrynager <kmager@wisc.edu>
2017-02-06 01:47:17 -0600

    Update ConversantChallenge.java

--- ConversantChallenge.java
+++ ConversantChallenge.java
@@ -17,20 +17,7 @@ import java.io.PrintWriter;
  * 
  * After plotting the data on a simple scatter plot to visualize the information,
  * I deducted that one large ArrayList for the data instead of a HashMap would have 
- * been simplier to use.
- * 
- * I made a few methods near the bottom of this class which calculate some information,
- * but the graph better shows the trends of the data. These methods include: average
- * value per DC, max value per DC and min value per DC (these can be used to find range).
- * 
- * Although I spent more time graphing the data, a helpful method to find trends 
- * would be to find the inflection points of the data center's info and compare them.
- * 
- * I can see in the graph that data center I fluctuates the most consistently.
- * Data center S has similar inflection points as I, but the range of difference
- * from maxima to minima is smaller. Finally, A is the most consistent in values
- * across time. A has the smallest range from highest to lowest point and remains
- * around an average values of ~3592.98.
+ * been simpler to use.
  * 
  * @author Kathryn Ager
  */
